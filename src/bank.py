9ca8b739f93969f9a57707bcae3ed7eabc7ef499
Utkarsh Singh <Utkarsh.Singh@watchguard.com>
2023-03-05 19:43:36 +0530

    src: Unify input validation

--- src/bank.py
+++ src/bank.py
@@ -1,9 +1,27 @@
 from collections import deque
+from collections.abc import Callable
 from getpass import getpass
 
 from account import Account, BankAccount
 from exceptions import AccountNotFound, InsufficientBalance, InvalidPassword
 from person import Cashier, Customer, Manager, Person, get_id
+
+
+def get_validated_input(callback: Callable, *args, **kwargs):
+    try:
+        return callback(*args, **kwargs)
+    except AccountNotFound:
+        print("Account not found. Retry!")
+        return get_validated_input(callback, *args, **kwargs)
+    except InsufficientBalance:
+        print(f"Minimum balance must be ₹{BankAccount.min_balance}. Retry!")
+        return get_validated_input(callback, *args, **kwargs)
+    except InvalidPassword:
+        print("Invalid password. Retry!")
+        return get_validated_input(callback, *args, **kwargs)
+    except ValueError:
+        print("Invalid input. Retry!")
+        return get_validated_input(callback, *args, **kwargs)
 
 
 class Bank:
@@ -36,21 +54,11 @@ class Bank:
         return id
 
     def _authenticate(self) -> Person:
-        try:
-            id = int(input("Enter id: "))
-            person = self.get_person(id)
-            password = getpass("Password: ")
-            person.login(password)
-            return person
-        except AccountNotFound:
-            print("Account Not Found. Retry!")
-            return self._authenticate()
-        except InvalidPassword:
-            print("Invalid Password. Retry!")
-            return self._authenticate()
-        except ValueError:
-            print("Invalid id or password. Retry!")
-            return self._authenticate()
+        id = int(input("Enter id: "))
+        person = self.get_person(id)
+        password = getpass("Password: ")
+        person.login(password)
+        return person
 
     def _input_choice(self, low: int, high: int) -> int:
         choice = int(input(f"Enter choice: [{low}-{high}]: "))
@@ -58,19 +66,28 @@ class Bank:
             return choice
         return self._input_choice(low, high)
 
+    def _deposit(self, bank_acc: BankAccount) -> None:
+        amount = int(input("Enter amount: "))
+        bank_acc.deposit(amount)
+
     def _withdraw(self, bank_acc: BankAccount) -> None:
         amount = int(input("Enter amount: "))
-        try:
-            bank_acc.withdraw(amount)
-        except InsufficientBalance:
-            print("Insufficient Balance. Retry!")
-            self._withdraw(bank_acc)
+        bank_acc.withdraw(amount)
+
+    def _input_continue(self) -> bool:
+        choice = input("Do you want to continue(y/n): ")
+        if choice.startswith("y"):
+            return True
+        elif choice.startswith("n"):
+            return False
+        else:
+            raise ValueError
 
     def _manage_customer(self, customer: Customer) -> None:
         print(
-            "1. Edit details\n2. Deposit\n3. Withdraw\n4. Display passbook\n5. Back to login"
+            "1. Edit details\n2. Deposit\n3. Withdraw\n4. Print passbook\n5. Back to login"
         )
-        choice = self._input_choice(1, 5)
+        choice = get_validated_input(self._input_choice, 1, 5)
 
         if choice == 5:
             self.manage()
@@ -81,58 +98,48 @@ class Bank:
             self.add_pending_change(customer, change)
         elif choice == 2:
             acc = customer.get_bank_account()
-            amount = int(input("Enter amount: "))
-            acc.deposit(amount)
+            get_validated_input(self._deposit, acc)
         elif choice == 3:
             acc = customer.get_bank_account()
-            self._withdraw(acc)
+            get_validated_input(self._withdraw, acc)
         elif choice == 4:
             acc = customer.get_bank_account()
             passbook = acc.get_passbook()
             for statements in passbook:
                 print(statements)
 
-        if input("Do you want to continue(y/n): ").startswith("y"):
+        if get_validated_input(self._input_continue):
             self._manage_customer(customer)
 
     def _input_person(self, person: Person) -> tuple[int, Person]:
-        if not (isinstance(person, Cashier) or isinstance(person, Manager)):
-            pass
-        try:
-            name = input("Enter name: ")
-            age = int(input("Enter age: "))
-            gen = input("Enter gender: ")
-            password = getpass("Enter password: ")
-            if isinstance(person, Manager):
-                return person.add_cashier(name, age, gen, password)
-            balance = int(input("Enter balance: "))
-            return person.add_customer(name, age, gen, password, balance)
-        except InsufficientBalance:
-            print("Insufficent Balance. Retry!")
-            return self._input_person(person)
-        except ValueError:
-            print("Invalid input format for age or balance. Retry!")
-            return self._input_person(person)
+        name = input("Enter name: ")
+        age = int(input("Enter age: "))
+        gen = input("Enter gender: ")
+        password = getpass("Enter password: ")
+        if isinstance(person, Manager):
+            return person.add_cashier(name, age, gen, password)
+        balance = int(input("Enter balance: "))
+        return person.add_customer(name, age, gen, password, balance)
 
     def _manage_cashier(self, cashier: Cashier) -> None:
         print("1. Add customer\n2. Back to login")
-        choice = self._input_choice(1, 2)
+        choice = get_validated_input(self._input_choice, 1, 2)
 
         if choice == 2:
             self.manage()
             return
 
         if choice == 1:
-            id, customer = self._input_person(cashier)
+            id, customer = get_validated_input(self._input_person, cashier)
             print(f"Customer with id {id} added to database.")
             self.add_person(id, customer)
 
-        if input("Do you want to continue(y/n): ").startswith("y"):
+        if get_validated_input(self._input_continue):
             self._manage_cashier(cashier)
 
     def _manage_manager(self, manager: Manager) -> None:
         print("1. Add cashier\n2. Process changes\n3. Back to login")
-        choice = self._input_choice(1, 3)
+        choice = get_validated_input(self._input_choice, 1, 3)
 
         if choice == 3:
             self.manage()
@@ -150,11 +157,11 @@ class Bank:
                 if not manager.process_change(customer, change):
                     self.add_pending_change(customer, change)
 
-        if input("Do you want to continue(y/n): ").startswith("y"):
+        if get_validated_input(self._input_continue):
             self._manage_manager(manager)
 
     def manage(self) -> None:
-        person = self._authenticate()
+        person = get_validated_input(self._authenticate)
         name = person.get_details()["name"]
         if isinstance(person, Customer):
             acc = person.get_bank_account()
