98e3516b1c2eaf95266e2726fe435d16bd4e4c2c
Utkarsh Singh <Utkarsh.Singh@watchguard.com>
2023-04-16 16:03:52 +0530

    backend: Add authorization through JWT tokens

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 from os import getenv
 
-from flask import Flask
-from flask_smorest import Api
-
 import resources
+from blocklist import BLOCKLIST
 from db import db
+from flask import Flask, jsonify
+from flask_jwt_extended import JWTManager
+from flask_smorest import Api
 
 
 def create_app(db_url=None):
@@ -22,6 +23,43 @@ def create_app(db_url=None):
         "DATABASE_URL", "sqlite:///data.db"
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = "secret"
+
+    jwt = JWTManager(app)
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
 
     db.init_app(app)
 
