3ff126753eda49e379595798040ca6b8bf4bbdc3
Utkarsh Singh <Utkarsh.Singh@watchguard.com>
2023-04-16 16:25:53 +0530

    backend: Add authorization to change endpoint

--- backend/resources/change.py
+++ backend/resources/change.py
@@ -1,5 +1,6 @@
 from db import db
 from flask.views import MethodView
+from flask_jwt_extended import get_jwt, jwt_required
 from flask_smorest import Blueprint, abort
 from models import ChangeModel, ChangeStatus, CustomerModel
 from resources.schemas import ChangeSchema, CommentSchema, PlainChangeSchema
@@ -17,9 +18,14 @@ class ChangeList(MethodView):
 
 @blp.route("/customers/<int:customer_id>/requestChange")
 class RequestChange(MethodView):
+    @jwt_required()
     @blp.arguments(PlainChangeSchema)
     @blp.response(201, ChangeSchema)
     def post(self, change_data, customer_id):
+        claim = get_jwt()
+        if claim["role"] != "customer" or claim["sub"] != customer_id:
+            abort(401, "Unauthorized")
+
         if change_data["old"].keys() != change_data["new"].keys():
             abort(400, message="Old and new request should contain same keys.")
 
@@ -45,8 +51,13 @@ class RequestChange(MethodView):
 
 @blp.route("/changes/<int:change_id>/accept")
 class AcceptChange(MethodView):
+    @jwt_required()
     @blp.response(200, ChangeSchema)
     def post(self, change_id):
+        claim = get_jwt()
+        if claim["role"] != "manager":
+            abort(401, "Unauthorized")
+
         change = ChangeModel.query.get_or_404(change_id)
         if change.status != ChangeStatus.pending:
             abort(409, message="Change not in an pending state.")
@@ -69,9 +80,14 @@ class AcceptChange(MethodView):
 
 @blp.route("/changes/<int:change_id>/reject")
 class RejectChange(MethodView):
+    @jwt_required()
     @blp.arguments(CommentSchema)
     @blp.response(200, ChangeSchema)
     def post(self, comment_data, change_id):
+        claim = get_jwt()
+        if claim["role"] != "manager":
+            abort(401, "Unauthorized")
+
         change = ChangeModel.query.get_or_404(change_id)
         if change.status != ChangeStatus.pending:
             abort(409, message="Change not in an pending state.")
